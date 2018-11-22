d5ed27ac45ffa26dd163267e7826b7edfd4b658c
narcoticfresh <dn@xbe.ch>
2018-11-22 16:39:12 +0100

    add our first lexer and parser to try

---
+++ src/Lexer.php
@@ -0,0 +1,115 @@
+<?php
+namespace AnalyticsConverter;
+
+use Doctrine\Common\Lexer\AbstractLexer;
+
+class Lexer extends AbstractLexer {
+
+    public const T_NONE              = 1;
+
+    public const T_COMMA = 2;
+    public const T_DOUBLEQUOTE = 3;
+    public const T_DOUBLEPOINT = 4;
+    public const T_NUMBER = 5;
+    public const T_CLOSE_PARENTHESIS = 6;
+    public const T_OPEN_PARENTHESIS  = 7;
+    public const T_STRING = 8;
+
+    public const T_OPEN_CURLY_BRACE  = 18;
+    public const T_CLOSE_CURLY_BRACE = 19;
+    public const T_OPEN_BRACKET  = 20;
+    public const T_CLOSE_BRACKET = 21;
+
+    public const T_COMMENT_PARAMSTART = 100;
+    public const T_COMMENT_PARAMEND = 101;
+    public const T_COMMENT_NOUSE = 102;
+
+    private $commentRegexes = [
+        //'\\/\* param\:([a-z0-9]*) \*\\/' => self::T_COMMENT_PARAMSTART,
+        '\\/\* param\:([a-z0-9]*) \*\\/' => self::T_COMMENT_PARAMSTART,
+        '\\/\* endparam \*\\/' => self::T_COMMENT_PARAMEND,
+        '\\/\*(.*)\*\\/' => self::T_COMMENT_NOUSE
+    ];
+
+    /**
+     * Creates a new query scanner object.
+     *
+     * @param string $input A query string.
+     */
+    public function __construct($input)
+    {
+        $this->setInput($input);
+    }
+
+    protected function getModifiers()
+    {
+        return 'im';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getCatchablePatterns()
+    {
+        return array_merge(
+            [
+            '(?:[0-9]+(?:[\.][0-9]+)*)(?:e[+-]?[0-9]+)?', // numbers
+            "\"(?:[^\"]|'')*\"" // quoted strings
+            ],
+            array_keys($this->commentRegexes)
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getNonCatchablePatterns()
+    {
+        return ['\s+', '(.)'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getType(&$value)
+    {
+        $type = self::T_NONE;
+
+        switch (true) {
+            case (is_numeric($value)):
+                return self::T_NUMBER;
+            case ($value[0] === "\""):
+                $value = str_replace("''", "'", substr($value, 1, strlen($value) - 2));
+                return self::T_STRING;
+            case ($value === '('):
+                return self::T_OPEN_PARENTHESIS;
+            case ($value === ')'):
+                return self::T_CLOSE_PARENTHESIS;
+            case ($value === '{'):
+                return self::T_OPEN_CURLY_BRACE;
+            case ($value === '}'):
+                return self::T_CLOSE_CURLY_BRACE;
+            case ($value === '['):
+                return self::T_OPEN_BRACKET;
+            case ($value === ']'):
+                return self::T_CLOSE_BRACKET;
+            case ($value === ','):
+                return self::T_COMMA;
+            case ($value === '"'):
+                return self::T_DOUBLEQUOTE;
+            case ($value === ':'):
+                return self::T_DOUBLEPOINT;
+
+            // Default
+            default:
+                foreach ($this->commentRegexes as $regex => $regexType) {
+                    if (preg_match('/'.$regex.'/i', $value)) {
+                        return $regexType;
+                    }
+                }
+        }
+
+        return $type;
+    }
+
+}
---
+++ src/Parser.php
@@ -0,0 +1,86 @@
+<?php
+namespace AnalyticsConverter;
+
+class Parser
+{
+    private $lexer;
+
+    private $lines = [];
+
+    private $currentLevel = 1;
+
+    public function __construct($input)
+    {
+        $this->lexer = new Lexer($input);
+    }
+
+
+
+    public function parse()
+    {
+        //$this->lexer->setInput($string);
+        $this->lexer->moveNext();
+
+        while (true) {
+            if (!$this->lexer->lookahead) {
+                break;
+            }
+
+            $this->lexer->moveNext();
+            $thisLine = '';
+
+            switch ($this->lexer->token['type']) {
+                case Lexer::T_OPEN_PARENTHESIS:
+                case Lexer::T_OPEN_BRACKET:
+                case Lexer::T_OPEN_CURLY_BRACE:
+                    $thisLine = '[';
+                    $this->currentLevel++;
+                    break;
+                case Lexer::T_CLOSE_PARENTHESIS:
+                case Lexer::T_CLOSE_BRACKET:
+                case Lexer::T_CLOSE_CURLY_BRACE:
+                    $thisLine = ']';
+                    $this->currentLevel--;
+                    break;
+                case Lexer::T_STRING:
+                    $thisLine = "'".str_replace("'", "\'", $this->lexer->token['value'])."'";
+                    break;
+                case Lexer::T_DOUBLEPOINT:
+                    $thisLine = '=>';
+                    break;
+                case Lexer::T_COMMENT_PARAMSTART:
+                    $paramName = $this->lexer->lookahead['value'];
+                    $thisLine = '/* START PARAM '.$paramName. '*/ $param ';
+                    // scroll to end of param
+                    while ($this->lexer->token['type'] != Lexer::T_COMMENT_PARAMEND) {
+                        $this->lexer->moveNext();
+                    }
+                    $thisLine .= ' /* PARAM END */ ';
+
+                    break;
+                case Lexer::T_COMMENT_NOUSE:
+                    // skip
+                    $this->lexer->moveNext();
+                    // IGNORE THIS
+                    break;
+                default:
+                    $thisLine = $this->lexer->token['value'];
+                    // nope
+            }
+
+            $this->lines[] = $this->getLevelIndent() . $thisLine;
+
+            //echo $this->lexer->token['value'].PHP_EOL;
+        }
+
+        return implode(PHP_EOL, $this->lines);
+    }
+
+    private function getLevelIndent()
+    {
+        return str_repeat(' ', $this->currentLevel * 2);
+    }
+
+
+    // ...
+}
