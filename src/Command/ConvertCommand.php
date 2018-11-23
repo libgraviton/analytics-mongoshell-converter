40071faff3ba54bd0aee47b1057207b58c7a90ba
narcoticfresh <dn@xbe.ch>
2018-11-23 13:21:10 +0100

    add a command

---
+++ src/Command/ConvertCommand.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * convert command
+ */
+namespace AnalyticsConverter\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+
+/**
+ * @author   List of contributors <https://github.com/libgraviton/compose-transpiler/graphs/contributors>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://swisscom.ch
+ */
+class ConvertCommand extends Command
+{
+
+    /**
+     * Configures the current command.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('convert')
+            ->setDescription('Convert analytics')
+            ->addArgument(
+                'scanDir',
+                InputArgument::REQUIRED,
+                'Which directory to scan'
+            )
+            ->addArgument(
+                'outDir',
+                InputArgument::REQUIRED,
+                'Where to put generated classes'
+            )
+			->addOption(
+				'mongoDateClass',
+				'm',
+				InputOption::VALUE_OPTIONAL,
+				'Override which Mongo Date class is used'
+			)
+			->addOption(
+				'classNamespace',
+				'c',
+				InputOption::VALUE_OPTIONAL,
+				'Override the generated class namespace'
+			);
+    }
+
+    /**
+     * Executes the current command.
+     *
+     * @param InputInterface  $input  User input on console
+     * @param OutputInterface $output Output of the command
+     *
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+		$fs = new Filesystem();
+		$finder = new Finder();
+		$finder
+			->files()
+			->in($input->getArgument('scanDir'))
+			->path('/\/analytics\//i')
+			->name('*.js')
+			->notName('_*')
+			->sortByName();
+
+		foreach ($finder as $file) {
+			$parser = new \AnalyticsConverter\Parser($file->getContents());
+			if (!is_null($input->getOption('mongoDateClass'))) {
+				$parser->setMongoDateClass($input->getOption('mongoDateClass'));
+			}
+
+			$classGenerator = new \AnalyticsConverter\ClassGenerator(
+				$file->getFilename(),
+				$parser->parse(),
+				$parser->getConditionals(),
+				$parser->getConditionalParamMap()
+			);
+
+			if (!is_null($input->getOption('classNamespace'))) {
+				$classGenerator->setClassNamespace($input->getOption('classNamespace'));
+			}
+
+			$targetFileName = $input->getArgument('outDir').'/'.$classGenerator->getName().'.php';
+
+			$fs->dumpFile($targetFileName, $classGenerator->generate());
+			echo "wrote ".$targetFileName.PHP_EOL;
+		}
+    }
+}
